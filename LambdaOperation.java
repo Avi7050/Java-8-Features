faab95d2ba89cb0d25a95913ce6704709f47f3b6
Avi7050 <avi7050416623@gmail.com>
2024-03-14 14:49:11 +0530

    lambda file

---
+++ LambdaOperation.java
@@ -0,0 +1,9 @@
+interface Operation{
+    void addition(int num1, int num2);
+}
+public class LambdaOperation {
+    public static void main(String[] args) {
+        Operation op = (num1,num2) -> System.out.println("Addition: " +(num1 + num2));
+        op.addition(10, 20);
+    }
+}
